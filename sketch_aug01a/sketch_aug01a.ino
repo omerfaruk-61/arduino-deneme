840d7e455ca7331397f6445a73b33990e54e770a
omerfaruk-61 <141166985+omerfaruk-61@users.noreply.github.com>
2023-08-01 19:29:01 +0300

    DENEME 1

---
+++ sketch_aug01a/sketch_aug01a.ino
@@ -0,0 +1,23 @@
+
+
+
+int sag_on = 1
+int sag_arka = 2
+int sol_on = 3
+int
+int
+int
+
+void setup() {
+  
+  
+  pinMode ( sag_on , INPUT);
+
+}
+
+void loop() {
+
+
+  digitalRead( sag_on , HIGH);
+
+}
